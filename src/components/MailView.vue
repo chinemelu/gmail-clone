e289a0bd8023ae6e9a3cb43bf5dc3d5cacf698d8
chinemelu <chinemelunwosu@gmail.com>
2021-12-16 11:44:36 +0100

    - create MailTable and MailView components - delete unnecessary composables

---
+++ src/components/MailView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="email-display">
+    <h2 class="mb-0">Subject: <strong>{{ email.subject }}</strong></h2>
+    <div><em>From {{ email.from }} on {{ format(new Date(email.sentAt), 'MMM do yyyy') }}</em></div>
+    <div v-html="marked(email.body)"/>
+  </div>
+</template>
+
+<script setup>
+  import { format } from 'date-fns'
+  import marked from 'marked'
+
+  const props = defineProps({
+    email: {
+      type: Object,
+      required: true
+    },
+  })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
